8f716a22a12f8139a81c18b3fa714f906b20c44d
Robert Tubman <roberttsunemichi.tubman@gmail.com>
2024-03-21 17:56:32 +1300

    Clean and fix code - Remove redundant code - Clean commented code sections - Fix spawner collider, assert parent object valid error, by adding a mesh in addition to the collider sphere - Fix spawner collider in the stage by replacing the existing blueprint with the updated class - Pre-emptive fix spawner box, by adding mesh to existing blueprint in stage

--- Source/LensChroma/CreatureSpawnCollider.h
+++ Source/LensChroma/CreatureSpawnCollider.h
@@ -2,8 +2,11 @@
 
 #pragma once
 
+// Required
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+
+// Required generated
 #include "CreatureSpawnCollider.generated.h"
 
 UCLASS()
@@ -15,14 +18,21 @@ public:
 	// Sets default values for this actor's properties
 	ACreatureSpawnCollider();
 
+	// Use this to have something to grab the object easier in the world
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UStaticMeshComponent* StaticMesh;
+
+	// The actual collider component
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	class USphereComponent* ColliderSphere;
 
+	// The blueprint handles for the spawn target location and the spawn actor
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	TSubclassOf<AActor> CreatureSpawnerBox;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	TSubclassOf<AActor> AberrantCreature;
 
+	// Collider control variables
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	bool bShouldDestroy = false;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
@@ -33,9 +43,7 @@ protected:
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
+	// On overlap function to handle the spawning
 	UFUNCTION()
 	virtual void OnOverlapBegin(
 		UPrimitiveComponent* OverlappedComponent,
