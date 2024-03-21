0ecc9a8ef0a1ae8c69ba25d8eabaf7d0f4568ec9
Robert Tubman <roberttsunemichi.tubman@gmail.com>
2024-03-21 17:36:42 +1300

    Clean up code - Remove redundant code - Remove commented code sections

--- Source/LensChroma/BaseGameMode.cpp
+++ Source/LensChroma/BaseGameMode.cpp
@@ -1,26 +1,34 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+// Header
 #include "BaseGameMode.h"
+
+// Game instance header
 #include "GameInstanceClass.h"
+
+// Gameplay statics functions 
 #include <Kismet/GameplayStatics.h>
 
+// Handles to the player character and the strange camera classes
 #include "FPSPlayerCharacter.h"
 #include "StrangeCamera.h"
-#include "CameraLens.h"
-#include "Camera/CameraComponent.h"
 
+/*
+ * Things to do when the game begins
+ */
 void ABaseGameMode::BeginPlay()
 {
 	Super::BeginPlay();
 
+	// Get a handle to the player character to get the save gmae load function
 	AFPSPlayerCharacter* ThePlayer = Cast<AFPSPlayerCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+	// If the handle was set
 	if (ThePlayer)
 	{
-		//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("BGM Loading Player")));
+		// Load the player character state
 		if (ThePlayer->LoadPlayer())
 		{
-			// Get the strange camera mesh and material
+			// Get the strange camera
 			AStrangeCamera* StrangeCamera = Cast<AStrangeCamera>(UGameplayStatics::GetActorOfClass(GetWorld(), AStrangeCamera::StaticClass()));
 
 			// Get the strange camera mesh and apply it to the player attach position
@@ -33,47 +41,31 @@ void ABaseGameMode::BeginPlay()
 	}
 }
 
+/*
+ * The override function for selecting the player start location
+ */
 AActor* ABaseGameMode::ChoosePlayerStart_Implementation(AController* Player)
 {
+	// Get the game instance
 	UGameInstanceClass* GameInstance = Cast<UGameInstanceClass>(GetGameInstance());
+
+	// Retrieve the tag of the start location we want
 	FName TTag = GameInstance->TeleportationTag;
 
-	if (TTag == FName("None"))
-	{
-		//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Got TTag:None, should set a default player start tag.")));
-		TTag = FName("TheStartPosition00");
-	}
+	// Set the start location tag we want if there is no tag set in the game instance
+	if (TTag == FName("None")) TTag = FName("TheStartPosition00");
 
-	//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Teletag DBG: %s"), *TTag.ToString()));
-
+	// Get all the player start locations from the world
 	TArray<AActor*> ActorClasses;
 	UGameplayStatics::GetAllActorsOfClass(GetWorld(), PlayerStartBP, ActorClasses);
-
+	
+	// For each actor that was found
 	for (AActor* SingleActor : ActorClasses)
 	{
-		if (SingleActor->ActorHasTag(TTag))
-		{
-			return SingleActor;
-		}
+		// If the actor has the tag we want, then use this start location
+		if (SingleActor->ActorHasTag(TTag)) return SingleActor;
 	}
 
-	ActorClasses[0]->Tags.Add(FName("FAILURE")); // DEBUG, Can remove this
+	// Otherwise no start location with the required tag was found. Use the first actor found
 	return ActorClasses[0];
-}
-
-void ABaseGameMode::InitPlayerSavedData()
-{
-	UE_LOG(LogTemp, Warning, TEXT("Initializing the player save data"));
-
-	if (UGameplayStatics::DoesSaveGameExist("PlayerData", 0))
-	{
-		UGameplayStatics::LoadGameFromSlot("PlayerData", 0);
-
-		// Level info processing
-		SavePlayerObject = Cast<USaveGamePlayer>(UGameplayStatics::LoadGameFromSlot("PlayerData", 0));
-	}
-	else
-	{
-		UGameplayStatics::CreateSaveGameObject(PlayerSaveData);
-	}
 }
--- Source/LensChroma/Humanoid005AnimInstance.cpp
+++ Source/LensChroma/Humanoid005AnimInstance.cpp
@@ -1,55 +1,62 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+// Header
 #include "Humanoid005AnimInstance.h"
-//#include "BaseCharacterClass.h"
+#include "BaseCharacterClass.h"
+
+// Character handles headers
 #include "GameFramework/Pawn.h"
 #include "GameFramework/CharacterMovementComponent.h"
 
+/*
+ * Initialization for the animation instance
+ */
 void UHumanoid005AnimInstance::NativeInitializeAnimation()
 {
 	Super::NativeInitializeAnimation();
 
+	// Set the character handle if not set
 	if (Pawn == nullptr)
 	{
 		Pawn = TryGetPawnOwner();
-		if (Pawn)
-		{
-			BaseCharacterClass = Cast<ABaseCharacterClass>(Pawn);
-		}
+		if (Pawn) BaseCharacterClass = Cast<ABaseCharacterClass>(Pawn);
 	}
 }
 
+/*
+ * Update the animation properties which are used in the animation of the humanoid npc character
+ */
 void UHumanoid005AnimInstance::UpdateAnimationProperties()
 {
-	if (Pawn == nullptr)
-	{
-		Pawn = TryGetPawnOwner();
-	}
+	// Set the character handle if not set
+	if (Pawn == nullptr) Pawn = TryGetPawnOwner();
 
+	// If the pawn has been set
 	if (Pawn)
 	{
+		// Get the movement state
 		FVector Speed = Pawn->GetVelocity();
 		FVector LateralSpeed = FVector(Speed.X, Speed.Y, 0.f);
-
 		MovementSpeed = LateralSpeed.Size();
-		//GEngine->AddOnScreenDebugMessage(40, 1.f, FColor::Black, FString::Printf(TEXT("BCC: MovingSpeed: %f"), MovementSpeed));
 
 		bIsInAir = Pawn->GetMovementComponent()->IsFalling();
 
+		// Set the handle if the character handle is not set
 		if (BaseCharacterClass == nullptr)
 		{
 			BaseCharacterClass = Cast<ABaseCharacterClass>(Pawn);
 		}
 
+		// If the character handle is set
 		if (BaseCharacterClass)
 		{
+			// Get the character stat
 			if (BaseCharacterClass->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0)
 				bIsAccelerating = true;
 			else
 				bIsAccelerating = false;
 
-			bIsFleeing = BaseCharacterClass->bIsFleeing;
+			// Set the character is dead state for animation
 			bIsDead = BaseCharacterClass->bIsDead;
 		}
 	}
